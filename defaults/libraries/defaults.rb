351c76a504d9f534c3cf324a4fc768b20a5f8e6b
Joshua Peek <josh@joshpeek.com>
2010-08-28 00:21:07 -0500

    Move Provider and Resource classes into libraries/

--- defaults/libraries/defaults.rb
+++ defaults/libraries/defaults.rb
@@ -1,3 +1,32 @@
+require 'chef/resource'
+
+class Chef::Resource::Defaults < Chef::Resource
+  def initialize(domain, key, run_context = nil)
+    super("#{domain} #{key}", run_context)
+
+    @domain = domain
+    @key = key
+    @value = nil
+
+    @resource_name = :defaults
+    @action = "run"
+    @allowed_actions.push(:run)
+  end
+
+  def domain(arg = nil)
+    set_or_return(:domain, arg, :kind_of => [String])
+  end
+
+  def key(arg = nil)
+    set_or_return(:key, arg, :kind_of => [String])
+  end
+
+  def value(arg = nil)
+    set_or_return(:value, arg, {})
+  end
+end
+
+
 require 'chef/provider'
 
 class Chef::Provider::Defaults < Chef::Provider
