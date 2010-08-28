180e036ad52898f62b8e7a705205b0380d40d75a
Joshua Peek <josh@joshpeek.com>
2010-08-27 22:27:40 -0500

    Add launch metadata

---
+++ launch/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Joshua Peek"
+maintainer_email "josh@joshpeek.com"
+license          "MIT"
+description      "Provides launch_service definition"
+version          "0.1.0"
+
+provides "launch_service(:name)"
+
+supports "mac_os_x"
