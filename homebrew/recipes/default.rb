3da8ba9d93cc6488dd449687230cd94cca22baad
Joshua Peek <josh@joshpeek.com>
2010-08-27 23:28:01 -0500

    Add recipe for downloading homebrew if its missing

--- homebrew/recipes/default.rb
+++ homebrew/recipes/default.rb
@@ -1,6 +1,20 @@
-ruby_block "check homebrew" do
-  block do
-    result = `brew --version`
-    raise("brew not working: #{result}") unless result.strip.to_f >= 0.7
-  end
+directory node[:homebrew][:prefix] do
+  action :create
 end
+
+homebrew_tar = "#{Chef::Config[:file_cache_path]}/mxcl-homebrew.tar.gz"
+
+remote_file homebrew_tar do
+  source "http://github.com/mxcl/homebrew/tarball/master"
+  action :create_if_missing
+end
+
+execute "tar -xzf #{homebrew_tar} -C #{node[:homebrew][:prefix]} --strip 1" do
+  creates "#{node[:homebrew][:prefix]}/bin/brew"
+end
+
+execute "#{node[:homebrew][:prefix]}/bin/brew --version"
+
+package "git"
+
+execute "#{node[:homebrew][:prefix]}/bin/brew update"
