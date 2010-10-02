ccfbcba583074378f890bf0ddd6aa6220259ea67
Joshua Peek <josh@joshpeek.com>
2010-10-02 14:55:38 -0500

    Add Safari autofill preference to defaults cookbook

--- defaults/recipes/safari.rb
+++ defaults/recipes/safari.rb
@@ -1,3 +1,7 @@
 defaults "com.apple.safari", "IncludeDevelopMenu" do
   value node[:apple][:safari][:develop_menu]
 end
+
+defaults "com.apple.safari", "AutoFillPasswords" do
+  value node[:apple][:safari][:auto_fill_passwords]
+end
