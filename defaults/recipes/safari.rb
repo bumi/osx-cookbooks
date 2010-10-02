6a289317964463be9db4a58c535a5505b3cafe46
Joshua Peek <josh@joshpeek.com>
2010-10-02 15:17:47 -0500

    Add Safari download clearing preference to defaults cookbook

--- defaults/recipes/safari.rb
+++ defaults/recipes/safari.rb
@@ -1,7 +1,11 @@
+defaults "com.apple.safari", "AutoFillPasswords" do
+  value node[:apple][:safari][:auto_fill_passwords]
+end
+
 defaults "com.apple.safari", "IncludeDevelopMenu" do
   value node[:apple][:safari][:develop_menu]
 end
 
-defaults "com.apple.safari", "AutoFillPasswords" do
-  value node[:apple][:safari][:auto_fill_passwords]
+defaults "com.apple.safari", "DownloadsClearingPolicy" do
+  value node[:apple][:safari][:downloads_clearing_policy]
 end
