75eea7e73353e966304536a2efb047144be91812
Joshua Peek <josh@joshpeek.com>
2010-11-01 20:22:27 -0500

    Run mysql_install_db after install

--- mysql/recipes/default.rb
+++ mysql/recipes/default.rb
@@ -2,6 +2,11 @@ include_recipe "homebrew"
 
 package "mysql"
 
+execute "mysql_install_db" do
+  user node[:homebrew][:user]
+  creates "/usr/local/var/mysql"
+end
+
 launch_service "com.mysql.mysqld" do
   template_variables :user => node[:launch][:user]
 end
