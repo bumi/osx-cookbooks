e1d5fe00f22820950adf6825aec412902685d531
Joshua Peek <josh@joshpeek.com>
2010-11-17 21:19:54 -0600

    Use mysqld instead of mysqld_safe in launchd

--- mysql/recipes/default.rb
+++ mysql/recipes/default.rb
@@ -4,9 +4,9 @@ package "mysql"
 
 execute "mysql_install_db" do
   user node[:homebrew][:user]
-  creates "/usr/local/var/mysql"
+  creates "#{node[:homebrew][:prefix]}/var/mysql"
 end
 
 launch_service "com.mysql.mysqld" do
-  template_variables :user => node[:launch][:user]
+  template_variables :prefix => node[:homebrew][:prefix]
 end
