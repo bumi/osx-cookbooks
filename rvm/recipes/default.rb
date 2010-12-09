24d0fb4f6a938cfed2ef74198865feb03c09e201
Joshua Peek <josh@joshpeek.com>
2010-12-09 13:07:58 -0800

    Clone rvm as sudo

--- rvm/recipes/default.rb
+++ rvm/recipes/default.rb
@@ -6,7 +6,6 @@ rvm_git = "#{Chef::Config[:file_cache_path]}/rvm"
 prefix = node[:rvm][:prefix]
 
 execute "git clone https://github.com/wayneeseguin/rvm.git #{rvm_git}" do
-  user node[:rvm][:user]
   not_if { File.exist?(rvm_git) || File.exist?("#{prefix}rvm") }
 end
 
